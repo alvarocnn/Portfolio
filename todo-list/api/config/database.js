77dacafdbde4cfec13c1e7b52d493f787d04b905
alvarocnp <alvarocp39@gmail.com>
2026-02-10 12:04:50 +0100

    actualizacion

--- todo-list/api/config/database.js
+++ todo-list/api/config/database.js
@@ -1,18 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const { eventNames } = require("../models/user");
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect('mongodb+srv://alvaro:alvaro@cluster0.tetdq54.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true,
-            useFindAndModify: false
-        });
-        console.log('MongoDB connected');
-    } catch (error) {
-        console.error('MongoDB connection failed', error);
-        process.exit(1);
-    }
+	try {
+		await mongoose.connect(process.env.MONGO_URI, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useCreateIndex: true,
+			useFindAndModify: false,
+		});
+		console.log("MongoDB connected");
+	} catch (error) {
+		console.error("MongoDB connection failed", error);
+		process.exit(1);
+	}
 };
 
 module.exports = connectDB;
