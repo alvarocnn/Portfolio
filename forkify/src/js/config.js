2ed5ade0312f580ececd7eb99c921cd84552c105
alvarocp39 <alvarocp39@gmail.com>
2026-03-25 12:13:40 +0100

    modificando el proyecto y arreglando errores Forkify

--- forkify/src/js/config.js
+++ forkify/src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes';
-export const API_KEY = 'eb293983-48f5-428b-b48e-a0e9cea08a6d';
+export const API_KEY = 'eb293983-48f5-428b-b48e-a0e9cea08a6d'; // no importa que se vea la key :)
 export const TIMEOUT_SECS = 10;
 export const RESULTS_PER_PAGE = 10;
 export const MODAL_CLOSE_SEC = 2.5;
